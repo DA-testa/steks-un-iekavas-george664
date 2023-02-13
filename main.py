bc29995c2dc82ddaaf96e39118f982884ba441d4
Georgijs Jefimovs <118355218+181RBC180@users.noreply.github.com>
2023-02-13 23:19:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    text2=text[5:]
+    text2=text.replace(text[:5],"")
+    # print(text2)
     mismatch = find_mismatch(text2)
     if mismatch==0:
         print("Success")
